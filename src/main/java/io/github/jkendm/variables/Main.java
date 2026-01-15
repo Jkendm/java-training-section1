f83fbb9e0c9729453309008f1a2ddb5120f91447
Joy Kendi <joykendim@gmail.com>
2026-01-15 22:02:16 +0300

    Convert src folder from submodule to normal directory

---
+++ src/main/java/io/github/jkendm/variables/Main.java
@@ -0,0 +1,19 @@
+package io.github.jkendm.variables;
+
+public class Main {
+    public static void main(String[] args) {
+    // Syntax dataType variableName;
+       int score;
+       String message;
+       double price;
+       // Declaration + Assignment
+        score= 100;
+       message = "Hello";
+        price = 19.99;
+         // Print variables
+
+         System.out.println("Score: " + score);
+           System.out.println("Message: " + message);
+             System.out.println("Price: " + price);
+    }    
+}
---
+++ src/main/java/io/github/jkendm/variables/Primitive.java
@@ -0,0 +1,19 @@
+package io.github.jkendm.variables;
+
+public class Primitive {
+    public static void main(String[] args) {
+        // Integer types
+        int age = 25;
+        long bigNumber = 123456789L;
+        // Decimal types
+        float price = 19.99f;
+        double precise = 3.1415926535;
+        // Character and boolean
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    }
+
+}
