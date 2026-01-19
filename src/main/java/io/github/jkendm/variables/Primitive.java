f0e54e85a032b174cf303b82be1b004307b9a88c
Joy Kendi <joykendim@gmail.com>
2026-01-19 16:35:32 +0300

    Type Casting

--- src/main/java/io/github/jkendm/variables/Primitive.java
+++ src/main/java/io/github/jkendm/variables/Primitive.java
@@ -14,6 +14,21 @@ public class Primitive {
 
         System.out.println("Age: " + age);
         System.out.println("Grade: " + grade);
+
+        //Type casting example -> Widening conversion from smaller to larger data type
+        int intNumber =100;
+        double doubleNumber = intNumber; // Implicit casting
+
+        System.out.println("Integer Number: " + intNumber);
+        System.out.println("Double Number (after implicit casting): " + doubleNumber);
+
+        //Narrow casting from larger to smaller data type
+        double myDouble =45.50;
+        int myInt= (int) myDouble; // Explicit casting (the brackets is because we are forcing the conversion)
+        System.out.println("Double Value: " + myDouble);
+        System.out.println("Integer Value (after explicit casting): " + myInt);
+
+
     }
 
 }
