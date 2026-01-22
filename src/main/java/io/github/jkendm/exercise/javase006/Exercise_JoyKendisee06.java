6545fb55ab61ce878cc92351947aff2b220e1043
Joy Kendi <joykendim@gmail.com>
2026-01-22 17:32:16 +0300

    Methods

--- src/main/java/io/github/jkendm/exercise/javase006/Exercise_JoyKendisee06.java
+++ src/main/java/io/github/jkendm/exercise/javase006/Exercise_JoyKendisee06.java
@@ -1,27 +1,93 @@
 package io.github.jkendm.exercise.javase006;
 
-
+/* For this particular code I referenced the following link to deal with array
+https://www.w3schools.com/java/java_howto_find_smallest_array_el.asp
+https://www.w3schools.com/java/java_howto_largest_el_array.asp
+ */
 
 public class Exercise_JoyKendisee06 {
     public static void main(String[] args) {
-        int result1 = add(5, 3);
-        int result2 = multiply(result1, 2);
-        printResult(result2);
-        printResult(multiply(add(5, 3), 2));
-        String formatted = formatResult(calculateFinalScore(85, 90, 78));
-        System.out.println(formatted);
-        
+        // Takes an array of grades and prints the average
+        double[] grades = {80,99,55,40,30};
+
+        //printed Formatted report 
+        printGradeReport("Joy Kendi", grades);
+
+        // Looking for the average
+      double average = calculateAverage(grades);
+       System.out.println("The average grade: " + average );
+
+       // get grade score - pass the average through the methods and then sort to look for the grade
+        char gradeScore = getGradeScore(average);
+        System.out.println("The Score is  " + gradeScore);
+
+       double lowest = lowestGrade(grades);
+        System.out.println("Lowest Grade " + lowest);
+
+         double highest = highestGrade(grades);
+        System.out.println("Highest Grade " + highest);
+
     }
-    
 
-public static int add(int a, int b) { return a + b; }
-public static int multiply(int a, int b) { return a * b; }
-public static void printResult(int value) { 
-System.out.println("Result: " + value); 
-}
-public static double calculateFinalScore(double test1, double test2, double test3) {return (test1 + test2 + test3) / 3; }
-public static String formatResult(double score) {
-    return String.format("Final Score: %.2f", score);
-}
+    public static void printGradeReport(String name, double[] grades){
+        System.out.println("========Students Final Report Card=========");
+        System.out.println("Student:" + name);
+        System.out.println("Grades: "); 
+         for (int i = 0; i < grades.length; i++) {
+            System.out.print(grades[i]);
+
+        // Print comma except after last grade
+         if (i < grades.length - 1) {
+            System.out.print(", ");
+        }
+    }
+        System.out.println(); //prints an empty line to divide code
 }
 
+    public static double calculateAverage(double [] grades){
+        double sum = 0;
+        
+        for (double g:grades){ // loops through the elements of array
+            sum += g;
+         }
+         return sum/grades.length; //grades.length -> gets the length of the array , getting the average is calculating the sum/average
+
+    }
+
+    public static char getGradeScore(double score){
+        // using the if else statement to map the average grade to the score
+        if (score >= 80){
+            return 'A'; 
+        }else if (score >= 70){
+            return 'B';
+        }else if( score >= 60){
+            return 'C';
+        }else if(score >= 50){
+            return 'D';
+        } else {
+            return 'E';
+        }
+    }
+
+    public static double lowestGrade(double [] grades){
+        double lowestScore = grades [0];
+        for ( double g:grades){
+            if (lowestScore > g){
+                lowestScore=g;
+            }
+        }
+        return lowestScore;
+
+    }
+
+    public static double highestGrade(double [] grades){
+        double highestScore = grades [0];
+        for ( double g:grades){
+            if (g > highestScore){
+                highestScore=g;
+            }
+        }
+        return highestScore;
+
+    }
+}
--- src/main/java/io/github/jkendm/practice_exercise/Temperature.java
+++ src/main/java/io/github/jkendm/practice_exercise/Temperature.java
@@ -11,8 +11,8 @@ public class Temperature {
 
         // Celsius to Fahrenheit
         System.out.print("Enter temperature in Celsius: ");
-        double celsius = scanner.nextDouble();
-        double fahrenheitResult = celsiusToFahrenheit(celsius);
+        double celsius = scanner.nextDouble(); // waiting for the user to input
+        double fahrenheitResult = celsiusToFahrenheit(celsius); // takes the celsius value and converts it inside the method
         System.out.println("Celsius to Fahrenheit: " + fahrenheitResult);
 
         // Fahrenheit to Celsius
@@ -23,6 +23,8 @@ public class Temperature {
 
         scanner.close();
 
+        printTemperatureTable();
+
     }
     //converting celsius to Fahrenheit and returning the value
 
@@ -30,9 +32,23 @@ public class Temperature {
         return (celsius * 9.0/5.0) + 32 ; 
 
     }
-
+//Fahrenheit to celsius
     public static double fahrenheitToCelsius(double fahrenheit){
         return (fahrenheit - 32) * 5.0/9.0;
     }
+    
+    public static double celsiusToFahrenheit(int celsius){
+        return (celsius * 9/5) + 32 ; 
 
+    }
+
+    public static void printTemperatureTable(){
+        System.out.println("\nTemperature Conversion Table"); // prints in a new line
+        System.out.println("°C\t°F"); //\t - creates a tab
+        // using the for loop to print with the steps of 10
+        for (int celsius = 0; celsius <= 100; celsius += 10) {
+            double fahrenheit = celsiusToFahrenheit(celsius); //takes the conversion from the method
+            System.out.printf("%d\t%.1f%n", celsius, fahrenheit);
+        }
+    }
 }
